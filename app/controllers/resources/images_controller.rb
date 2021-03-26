5e998c9211ea06543fd4ad20897001ca2c74c705
Nicolas Lattemann <nicolas.lattemann@captaindata.co>
2021-03-26 12:20:10 +0100

    added the possibility to add a query to get requests

--- app/controllers/resources/images_controller.rb
+++
@@ -1,6 +0,0 @@
-class Resources::ImagesController < ApplicationController
-	def show
-		render :file => "app/assets/resources/%d/%s_%d.jpg" % [params[:id], params[:type], params[:id]]
-		# Glacon.find(params[:id]).name + '.jpg' 
-	end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'api/v1/glacons/:id/resources/:type', to: 'resources/images#show'
   
   Rails.application.routes.draw do
     namespace :api do
