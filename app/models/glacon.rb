daf8641e818957418082c539ad5deb421d896ab1
Nicolas Lattemann <nicolas.lattemann@captaindata.co>
2021-03-26 15:15:48 +0100

    no more negative quantiities

--- app/models/glacon.rb
+++ app/models/glacon.rb
@@ -6,9 +6,15 @@ class Glacon < ApplicationRecord
   before_create do
     self.uuid = SecureRandom.hex(4)
     self.slug = self.name.to_slug.normalize.to_s
+    self.quantity = self.quantity > 0 ? self.quantity : 0
 
-    self.header = self.header ? Base64.encode64(open(self.header).read).gsub("\n", "") : ''
-    self.thumbnail = self.thumbnail ? Base64.encode64(open(self.thumbnail).read).gsub("\n", "") :''
 
+    begin
+      self.header = self.header ? Base64.encode64(open(self.header).read).gsub("\n", "") : nil
+      self.thumbnail = self.thumbnail ? Base64.encode64(open(self.thumbnail).read).gsub("\n", "") : nil
+    rescue
+      self.header = nil
+      self.thumbnail = nil
+    end
   end
 end
